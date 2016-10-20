8e0a4a6903ea55fa8e9b41578778cf034db8d86b
Aleksey Selyavkin <aleksey.selyavkin@gmail.com>
2016-10-20 20:51:49 +0300

    Changed some animation effects

--- common.js
+++ common.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $("a.scroll").click(function () { 
       var elementClick = $(this).attr("href");
       var destination = $(elementClick).offset().top;
-      $("html,body").animate( { scrollTop: destination }, 1300 );
+      $("html,body").animate( { scrollTop: destination }, 1600 );
       return false;
     });
 
